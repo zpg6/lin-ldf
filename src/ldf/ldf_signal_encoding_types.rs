35f41bb7bf40f57e9cac1848a3a0340b8b6bd02c
Zach Grimaldi <zpg6@pitt.edu>
2024-10-21 22:49:51 -0400

    fix(v0.0.6): allow decimal signal offset

--- src/ldf/ldf_signal_encoding_types.rs
+++ src/ldf/ldf_signal_encoding_types.rs
@@ -136,7 +136,7 @@ pub fn parse_ldf_signal_encoding_types(s: &str) -> IResult<&str, Vec<LdfSignalEn
                     let (s, _) = skip_whitespace(s)?;
                     let (s, _) = tag(",")(s)?;
                     let (s, _) = skip_whitespace(s)?;
-                    let (s, offset) = take_while(|c: char| c.is_numeric() || c == '-')(s)?;
+                    let (s, offset) = take_while(|c: char| c.is_numeric() || c == '.' || c == '-')(s)?;
                     let (s, _) = skip_whitespace(s)?;
                     let (s, _) = tag(",")(s)?;
                     let (s, _) = skip_whitespace(s)?;
