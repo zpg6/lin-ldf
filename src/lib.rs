a7863c088c9ce0b8de97b5c7f7386024f37b9c1e
Zach Grimaldi <zpg6@pitt.edu>
2024-10-22 18:51:11 -0400

    docs(v0.0.16): example snippet prints frames and signals

--- src/lib.rs
+++ src/lib.rs
@@ -34,7 +34,6 @@
 //! LIN_protocol_version = "2.1" ;
 //! LIN_language_version = "2.1" ;
 //! LIN_speed = 19.2 kbps ;
-//! Channel_name = "DB";
 //!
 //! /* PARSING IGNORES BLOCK COMMENTS */
 //!
@@ -52,57 +51,48 @@
 //!     Signal6: 1, 0, Slave1, Master ;
 //! }
 //!
-//! Diagnostic_signals {
-//!     MasterReqB0: 8, 0 ;
-//!     MasterReqB1: 8, 0 ;
-//!     MasterReqB2: 8, 0 ;
-//!     MasterReqB3: 8, 0 ;
-//!     MasterReqB4: 8, 0 ;
-//!     MasterReqB5: 8, 0 ;
-//! }
-//!
 //! Frames {
-//!   Frame1: 0, Master, 8 {
-//!     Signal1, 0 ;
-//!     Signal2, 10 ;
-//!   }
-//!   Frame2: 0x16, Slave1, 8 {
-//!       Signal3, 0 ;
-//!       Signal4, 10 ;
-//!   }
+//!     Frame1: 0, Master, 8 {
+//!         Signal1, 0 ;
+//!         Signal2, 10 ;
+//!     }
+//!     Frame2: 0x16, Slave1, 8 {
+//!         Signal3, 0 ;
+//!         Signal4, 10 ;
+//!     }
 //! }
 //!
 //! Node_attributes {
-//!   Slave1{
-//!       LIN_protocol = "2.1" ;
-//!       configured_NAD = 0xB ;
-//!       initial_NAD = 0xB ;
-//!       product_id = 0x123, 0x4567, 8 ;
-//!       response_error = Signal1 ;
-//!       P2_min = 100 ms ;
-//!       ST_min = 0 ms ;
-//!       N_As_timeout = 1000 ms ;
-//!       N_Cr_timeout = 1000 ms ;
-//!       configurable_frames {
-//!           Frame1 ;
-//!           Frame2 ;  
-//!       }
-//!   }
-//!   Slave2{
-//!       LIN_protocol = "2.1" ;
-//!       configured_NAD = 0xC ;
-//!       initial_NAD = 0xC ;
-//!       product_id = 0x124, 0x4568, 0x66 ;
-//!       response_error = Signal2 ;
-//!       P2_min = 100 ms ;
-//!       ST_min = 0 ms ;
-//!       N_As_timeout = 1000 ms ;
-//!       N_Cr_timeout = 1000 ms ;
-//!       configurable_frames {
-//!           Frame1 ;
-//!           Frame2 ;
-//!       }
-//!   }
+//!    Slave1 {
+//!        LIN_protocol = "2.1" ;
+//!        configured_NAD = 0xB ;
+//!        initial_NAD = 0xB ;
+//!        product_id = 0x123, 0x4567, 8 ;
+//!        response_error = Signal1 ;
+//!        P2_min = 100 ms ;
+//!        ST_min = 0 ms ;
+//!        N_As_timeout = 1000 ms ;
+//!        N_Cr_timeout = 1000 ms ;
+//!        configurable_frames {
+//!            Frame1 ;
+//!            Frame2 ;  
+//!        }
+//!    }
+//!    Slave2 {
+//!        LIN_protocol = "2.1" ;
+//!        configured_NAD = 0xC ;
+//!        initial_NAD = 0xC ;
+//!        product_id = 0x124, 0x4568, 0x66 ;
+//!        response_error = Signal2 ;
+//!        P2_min = 100 ms ;
+//!        ST_min = 0 ms ;
+//!        N_As_timeout = 1000 ms ;
+//!        N_Cr_timeout = 1000 ms ;
+//!        configurable_frames {
+//!            Frame1 ;
+//!            Frame2 ;
+//!        }
+//!    }
 //! }
 //!
 //! Schedule_tables {
@@ -111,31 +101,17 @@
 //!         Frame2 delay 10 ms ;
 //!     }
 //! }
-//!
-//! Signal_encoding_types {
-//!     ENC_BOOL {
-//!         logical_value, 0, "FALSE" ;
-//!         logical_value, 1, "TRUE" ;
-//!     }
-//!     ENC_TEMP {
-//!         physical_value, 0, 1023, 0.2, -40, "degC" ;
-//!     }
-//!     ENC_RPM {
-//!         physical_value, 0, 1023, 10, 0, "rpm" ;
-//!     }
-//! }
-//!
-//! Signal_representation {
-//!     ENC_BOOL: Signal1, Signal2 ;
-//!     ENC_TEMP: Signal3, Signal4 ;
-//!     ENC_RPM: Signal5, Signal6 ;
-//! }
-//! "#; // ... rest of the LDF file
+//! "#;
 //!
 //! let parsed_ldf = parse_ldf(ldf).expect("Failed to parse LDF file");
-//! let lin_protocol_version = parsed_ldf.header.lin_protocol_version; // "2.1"
-//! let jitter = parsed_ldf.nodes.master.jitter; // "0.1 ms"
+//! let total_signal_count = parsed_ldf.signals.len(); // 6
 //!
+//! for frame in parsed_ldf.frames {
+//!     println!("Frame: {}", frame.frame_name);
+//!     for signal in frame.signals {
+//!         println!("\tSignal: {}", signal.signal_name);
+//!     }
+//! }
 //! ```
 
 mod ldf;
